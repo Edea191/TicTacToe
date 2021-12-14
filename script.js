d62c8155239316cbf665fcfa7aa28f06e20a46ae
Edea191 <deswid@interia.pl>
2021-12-14 20:20:07 +0100

    edit css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const playerAboxShadow = "3px 3px 5px #be7f48";
 const playerBboxShadow = "3px 3px 5px #FA8072";
 const playerAbutton= "#F4A460";
 const playerBbutton= "#FA8072";
-const mainBackgroundColor = "FFF5EE";
+const playerActiveBackgroundColor = "#f1e0d4";
 
 var playerX=true;
 var playerY=false; 
@@ -17,7 +17,7 @@ var a1=0, a2=0, a3=0, b1=0, b2=0, b3=0, c1=0, c2=0, c3=0;
 
 function clicker(position){
    /*document.getElementById(position).style.boxShadow=player_x_box_shadow;*/
-
+   document.getElementById(position).setAttribute("disabled", true);
    switch(position){
 
         case 'a1':
@@ -65,7 +65,7 @@ function clicker(position){
         playerY=true;
         turn = 2;
         document.getElementById("player_x").style.backgroundColor="white";
-        document.getElementById("player_y").style.backgroundColor=mainBackgroundColor;
+        document.getElementById("player_y").style.backgroundColor=playerActiveBackgroundColor;
 
         howIsWinner(a1,a2,a3,b1,b2,b3,c1,c2,c3);
 
@@ -76,7 +76,7 @@ function clicker(position){
         document.getElementById(position).style.backgroundColor=playerBbutton;
         document.getElementById(position).style.boxShadow=playerBboxShadow;
 
-        document.getElementById("player_x").style.backgroundColor=mainBackgroundColor;
+        document.getElementById("player_x").style.backgroundColor=playerActiveBackgroundColor;
         document.getElementById("player_y").style.backgroundColor="white";
 
         playerX=true;
@@ -94,57 +94,81 @@ function clicker(position){
 
 function howIsWinner(a1, a2, a3, b1, b2, b3, c1, c2, c3){
     if (a1== 1 && a2 == 1 && a3 == 1){
-        alert("Player A Winer");
+        alert("Player A Winner!");
+        window.location.reload(true);
     }
 
     else if(b1== 1 && b2 == 1 && a3== 1){
         alert("Player A Winner!");
+        window.location.reload(true);
     }
 
     else if(c1== 1 && c2 == 1 && c3== 1){
         alert("Player A Winner!");
+        window.location.reload(true);
     }
 
     else if(a1== 1 && b1 == 1 && c1== 1){
         alert("Player A Winner!");
+        window.location.reload(true);
     }
 
     else if(a2== 1 && b2 == 1 && c2== 1){
         alert("Player A Winner!");
+        window.location.reload(true);
     }
     else if(a3== 1 && b3 == 1 && c3== 1){
         alert("Player A Winner!");
+        window.location.reload(true);
     }
     else if(a1== 1 && b2 == 1 && c3 ==1){
-        alert("Payer A Winner");
+        alert("Player A Winner!");
+        window.location.reload(true);
+    } 
+    else if(a3== 1 && b2 == 1 && c1 ==1){
+        alert("Player A Winner!");
+        window.location.reload(true);
     } 
 
 
     else if (a1== 2 && a2 == 2 && a3 == 2){
-        alert("Player B Winer");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
 
     else if(b1== 2 && b2 == 2 && a3== 2){
-        alert("Player A Winner!");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
 
     else if(c1== 2 && c2 == 2 && c3== 2){
-        alert("Player A Winner!");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
 
     else if(a1== 2 && b1 == 2 && c1== 2){
-        alert("Player A Winner!");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
 
     else if(a2== 2 && b2 == 2 && c2== 2){
-        alert("Player A Winner!");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
     else if(a3== 2 && b3 == 2 && c3== 2){
-        alert("Player A Winner!");
+        alert("Player B Winner!");
+        window.location.reload(true);
     }
     else if(a1== 2 && b2 == 2 && c3 ==2){
-        alert("Payer A Winner");
+        alert("Payer B Winner!");
+        window.location.reload(true);
     } 
+
+    else if(a3== 2 && b2 == 2 && c1 ==2){
+        alert("Payer B Winner!");
+        window.location.reload(true);
+    } 
+
 
 
 }
